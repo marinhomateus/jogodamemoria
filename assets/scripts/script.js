a84ea39d09fce94bbf1fa3ec3b169e97a189e1a8
Mateus Marinho <mateus.marinho@hotmail.com.br>
2021-06-04 18:50:38 -0300

    shuffle

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,44 +1,67 @@
-const FRONT = 'card_front'
-const BACK = 'card_back'
+const FRONT = "card_front";
+const BACK = "card_back";
 
 let techs = [
-    'bootstrap',
-    'css',
-    'electron',
-    'firebase',
-    'html',
-    'javascript',
-    'jquerry',
-    'mongo', 
-    'node', 
-    'react'
+  "bootstrap",
+  "css",
+  "electron",
+  "firebase",
+  "html",
+  "javascript",
+  "jquerry",
+  "mongo",
+  "node",
+  "react",
 ];
 
-const criaCartas = (techs) => {
+let cards = null;
+
+const startGame = () => {
+    let cards = createCards(techs);
+    embaralha(cards);
+    console.log(cards);
+};
+
+
+const embaralha = (cards) => {
+    let index = cards.length;
+    let randomIndex = 0;
     
-    let cards=[];//array vazio que recebe todas as cartas
-
-    for (let tech of techs){ 
-        cards.push(criaPar(tech));  //pega cada elemento e gera o seu par
+    while (index !== 0){
+        randomIndex = Math.floor(Math.random() * index);
+        index--;
+        
+        [cards[randomIndex], cards[index]] = [cards[index], cards[randomIndex]];
     }
+};
 
-    console.log(cards.flatMap(pair => pair)); //separa os elementos no array
-}
+const createCards = (techs) => {
+    let cards = []; //array vazio que recebe todas as Cards
+    
+    for (let tech of techs) {
+        cards.push(criaPar(tech)); //pega cada elemento e gera o seu par
+    }
+    
+    return cards.flatMap((pair) => pair); //separa os elementos no array
+};
 
-const criaPar = (tech) =>{
-    return [{
-        id: geraId(tech),
-        icon: tech,
-        flipped: false,
-    }, {
-        id: geraId(tech),
-        icon: tech,
-        flipped: false,        
-    }]
-}
+const criaPar = (tech) => {
+    return [
+        {
+            id: geraId(tech),
+            icon: tech,
+            flipped: false,
+        },
+        {
+            id: geraId(tech),
+            icon: tech,
+            flipped: false,
+        },
+    ];
+};
 
 const geraId = (tech) => {
-    return tech + parseInt(Math.random()*1000);
-}
+    return tech + parseInt(Math.random() * 1000);
+};
 
-criaCartas(techs);
+startGame();
